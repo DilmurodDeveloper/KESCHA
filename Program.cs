09ccfa048f336d89a98fcebdd47ef839b249ff3c
Dilmurod <madirimovdilmurod2@gmail.com>
2023-07-30 20:04:23 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Ismingzni kiriting: ");
+string name = Console.ReadLine();
+Console.Write("Familyangizni kiriting: ");
+string lastname = Console.ReadLine();
+string greeting  = $"Salom, {name}  {lastname}";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Yoshingizni kiriting: ");
+string ageAsString = Console.ReadLine();
+System.Console.WriteLine("Konvertatsiya qilinmoqda...");
+int age =  Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Muvaffaqiyatli konvertatsiya qilindi! {age}");
+
+int KeschaYoshi = 5;
+int yoshFarqi = age - KeschaYoshi;
+
+System.Console.WriteLine($"sizning va kescha o'rtasidagi farq {yoshFarqi}");
