84b051c70f3a0529dc1898547a250a2535309b60
Dilmurod <madirimovdilmurod2@gmail.com>
2023-10-11 10:47:26 +0500

    Teach comparison

--- Program.cs
+++ Program.cs
@@ -15,3 +15,11 @@ int KeschaYoshi = 5;
 int yoshFarqi = age - KeschaYoshi;
 
 System.Console.WriteLine($"sizning va kescha o'rtasidagi farq {yoshFarqi}");
+
+System.Console.WriteLine($"The difference between your and Kescha's age is: {yoshFarqi}");
+System.Console.WriteLine($"You are older than or equal to KESCHA: {age >= KeschaYoshi}");
+System.Console.WriteLine($"You are older than to KESCHA: {age > KeschaYoshi}");
+System.Console.WriteLine($"You are younger than or equal to KESCHA: {age < KeschaYoshi}");
+System.Console.WriteLine($"You are younger than KESCHA: {age <= KeschaYoshi}");
+System.Console.WriteLine($"Your age is equal to KESCHA's age: {age == KeschaYoshi}");
+System.Console.WriteLine($"Your age is not equal to KESCHA's age: {age != KeschaYoshi}");
