6e6f9e5e4efb1a7ac21785786b904d010b330a22
Dilmurod <madirimovdilmurod2@gmail.com>
2024-02-04 22:45:08 +0500

    Kescha was further improved

--- Program.cs
+++ Program.cs
@@ -1,25 +1,26 @@
-﻿Console.Write("Ismingzni kiriting: ");
-string name = Console.ReadLine();
-Console.Write("Familyangizni kiriting: ");
-string lastname = Console.ReadLine();
-string greeting  = $"Salom, {name}  {lastname}";
-System.Console.WriteLine(greeting);
+﻿using KESCHA.Classes;
 
-System.Console.Write("Yoshingizni kiriting: ");
+// string password = "";
+// do
+// {
+//     System.Console.Write("Parolni kiriting: ");
+//     password = Console.ReadLine();
+// }while(password != "Dilmurod");
+
+Animal Kescha = new Animal(name: "Kescha", age: 5);
+
+Console.Write("Ismingzni kiriting: ");
+string userName = Console.ReadLine();
+Kescha.Greet(userName);
+
+Console.Write("Yoshingizni kiriting: ");
 string ageAsString = Console.ReadLine();
-System.Console.WriteLine("Konvertatsiya qilinmoqda...");
-int age =  Convert.ToInt32(ageAsString);
-System.Console.WriteLine($"Muvaffaqiyatli konvertatsiya qilindi! {age}");
 
-int KeschaYoshi = 5;
-int yoshFarqi = age - KeschaYoshi;
+Console.WriteLine("Konvertatsiya qilinmoqda...");
+int userAge =  Convert.ToInt32(ageAsString);
+Console.WriteLine($"Muvaffaqiyatli konvertatsiya qilindi! {userAge}");
 
-System.Console.WriteLine($"sizning va kescha o'rtasidagi farq {yoshFarqi}");
-
-System.Console.WriteLine($"The difference between your and Kescha's age is: {yoshFarqi}");
-System.Console.WriteLine($"You are older than or equal to KESCHA: {age >= KeschaYoshi}");
-System.Console.WriteLine($"You are older than to KESCHA: {age > KeschaYoshi}");
-System.Console.WriteLine($"You are younger than or equal to KESCHA: {age < KeschaYoshi}");
-System.Console.WriteLine($"You are younger than KESCHA: {age <= KeschaYoshi}");
-System.Console.WriteLine($"Your age is equal to KESCHA's age: {age == KeschaYoshi}");
-System.Console.WriteLine($"Your age is not equal to KESCHA's age: {age != KeschaYoshi}");
+Kescha.CalculateAgeDifferenceWithNoReturn(userAge);
+Kescha.PrintAgeDifference();
+Kescha.CompareAges(userAge);
+Kescha.TellAboutFriends(userName, userAge);
