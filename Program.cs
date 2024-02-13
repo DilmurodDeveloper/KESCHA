59ab5fe5252c07371510fd39983faa7b87462a2a
Dilmurod <madirimovdilmurod2@gmail.com>
2024-02-13 17:27:28 +0500

    update project

--- Program.cs
+++ Program.cs
@@ -1,26 +1,28 @@
 ﻿using KESCHA.Classes;
 
-// string password = "";
-// do
-// {
-//     System.Console.Write("Parolni kiriting: ");
-//     password = Console.ReadLine();
-// }while(password != "Dilmurod");
+namespace KESCHA
+{
+    class Program
+    {
+        static void Main()
+        {
+            Animal Kescha = new Animal(name: "Kescha", age: 5);
 
-Animal Kescha = new Animal(name: "Kescha", age: 5);
+            Console.Write("Ismingzni kiriting: ");
+            string userName = Console.ReadLine();
+            Kescha.Greet(userName);
 
-Console.Write("Ismingzni kiriting: ");
-string userName = Console.ReadLine();
-Kescha.Greet(userName);
+            Console.Write("Yoshingizni kiriting: ");
+            string ageAsString = Console.ReadLine();
 
-Console.Write("Yoshingizni kiriting: ");
-string ageAsString = Console.ReadLine();
+            Console.WriteLine("Konvertatsiya qilinmoqda...");
+            int userAge =  Convert.ToInt32(ageAsString);
+            Console.WriteLine($"Muvaffaqiyatli konvertatsiya qilindi! {userAge}");
 
-Console.WriteLine("Konvertatsiya qilinmoqda...");
-int userAge =  Convert.ToInt32(ageAsString);
-Console.WriteLine($"Muvaffaqiyatli konvertatsiya qilindi! {userAge}");
-
-Kescha.CalculateAgeDifferenceWithNoReturn(userAge);
-Kescha.PrintAgeDifference();
-Kescha.CompareAges(userAge);
-Kescha.TellAboutFriends(userName, userAge);
+            Kescha.CalculateAgeDifferenceWithNoReturn(userAge);
+            Kescha.PrintAgeDifference();
+            Kescha.CompareAges(userAge);
+            Kescha.TellAboutFriends(userName, userAge);
+        }
+    }
+}
